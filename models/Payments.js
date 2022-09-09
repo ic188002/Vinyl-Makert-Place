e2773077e98b4d76634e21c692e8a37f173298d6
Ivan Craig <ic188002@falmouth.ac.uk>
2022-09-09 10:38:10 +0100

    models

---
+++ models/Payments.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// Schema
+const paymentsSchema = mongoose.Schema({
+    billingAddress: {
+        streetName: {
+           type: String,
+           required: true,
+           minlenght: [6, 'need to be longer'],
+       },
+       townCity : {
+           type : String,
+           required: true,
+       },
+       houseNumber : {
+           type : Number,
+           required: true,
+       },
+       postcode : {
+           type : String,
+           required: true,
+       },
+
+    user: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+
+}, 
+
+});
+
+
+const Model = mongoose.model("Payments", paymentsSchema);
+
+
+module.exports = {Model};
