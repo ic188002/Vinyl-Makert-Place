6461056a95d2db6362dbf1a206917d0af6a2bb10
eskres <einar.skreslett@gmail.com>
2022-09-15 18:16:20 +0100

    changes to cart controller

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,57 @@
 $('#lookup-submit').submit(function lookup() {
     console.log("lookup-submit");
 })
+
+function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    const place = autocomplete.getPlace();
+    let address1 = "";
+    let postcode = "";
+  
+    // Get each component of the address from the place details,
+    // and then fill-in the corresponding field on the form.
+    // place.address_components are google.maps.GeocoderAddressComponent objects
+    // which are documented at http://goo.gle/3l5i5Mr
+    for (const component of place.address_components) {
+      // @ts-ignore remove once typings fixed
+      const componentType = component.types[0];
+  
+      switch (componentType) {
+        case "street_number": {
+          address1 = `${component.long_name} ${address1}`;
+          break;
+        }
+  
+        case "route": {
+          address1 += component.short_name;
+          break;
+        }
+  
+        case "postal_code": {
+          postcode = `${component.long_name}${postcode}`;
+          break;
+        }
+  
+        case "postal_code_suffix": {
+          postcode = `${postcode}-${component.long_name}`;
+          break;
+        }
+        case "locality":
+          document.querySelector("#locality").value = component.long_name;
+          break;
+        case "administrative_area_level_1": {
+          document.querySelector("#state").value = component.short_name;
+          break;
+        }
+        case "country":
+          document.querySelector("#country").value = component.long_name;
+          break;
+      }
+    }
+    address1Field.value = address1;
+    postalField.value = postcode;
+    address2Field.focus();
+  }
+  
+  window.initAutocomplete = initAutocomplete;
+  
